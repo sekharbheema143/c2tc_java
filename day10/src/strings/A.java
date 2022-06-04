39bfd333850ab42260efaf5b701b5928061ca923
Admin <Admin@DESKTOP-KCJO4M4>
2022-06-04 13:29:57 +0530

    first commit

---
+++ day10/src/strings/A.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class A {
+	public static void main(String args[]) {
+		String str = "programming";//string literal
+		String s = new String("world");//using new keyword
+		System.out.println(str);
+		System.out.println(s);
+	}
+
+}
