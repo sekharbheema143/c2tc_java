8a24eef10da13aa633a7fb259fa68475bd2df78f
Admin <Admin@DESKTOP-KCJO4M4>
2022-07-11 09:49:42 +0530

    first commit

---
+++ day 23/src/collections/Main9.java
@@ -0,0 +1,23 @@
+package collections;
+import java.util.*;
+
+
+
+public class Main9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List <Integer> l = new ArrayList<Integer>();
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		l.add(4);
+		l.add(5);
+		l.add(6);
+		l.add(4);
+		System.out.println(l);
+		
+
+	}
+
+}
