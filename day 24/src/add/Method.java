3aa26cddd9b201d3ce5bf897cd6c4a0b2d29059a
Admin <Admin@DESKTOP-KCJO4M4>
2022-07-17 13:57:04 +0530

    first commit

---
+++ day 24/src/add/Method.java
@@ -0,0 +1,24 @@
+package add;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> val = new ArrayList<>();
+		val.add(0);
+		val.add(11);
+		val.add(23);
+		val.add(34);
+		val.add(45);
+		for(Integer e : val)
+		{
+			System.out.print(e);
+		}
+		System.out.println("ArrayList : " + val);
+
+	}
+
+}
---
+++ day 24/src/addall/Method2.java
@@ -0,0 +1,20 @@
+package addall;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Method2 {
+
+	public static void main(String[] args) 
+	{
+		List<String> val1 = new ArrayList<>();
+			val1.add("corona can be stopped");
+		List<String> val2 = new ArrayList<>();
+			val2.add("stay at home");
+			val1.addAll(val2);
+			System.out.println("val1");
+		
+
+	}
+
+}
